5e9cd81e3f87528baac5adb771e4d1465e3b5503
Kevin Wellmann <kevin@wellmann.io>
2023-08-14 11:58:12 +0200

    fix react error after 6.3 update

--- src/components/Wrapper/EditWrapper.js
+++ src/components/Wrapper/EditWrapper.js
@@ -1,60 +1,24 @@
 // WordPress dependencies.
-const { withDispatch } = wp.data;
-const { tokenList: TokenList, serverSideRender: ServerSideRender } = wp;
+const { serverSideRender: ServerSideRender } = wp;
 
 // Local dependencies.
-import { convertToBem } from '../../utils';
 import EditContext from '../EditContext';
 
-const getEdit = ({ blockNamespace, additionalClassNames, editFunction }) => (
-  withDispatch(( dispatch, { className, clientId } ) => {
-    if (additionalClassNames) {
-      const list = new TokenList(className);
-      list.remove(className.split(' ')[0]); // Remove blocks default class name.
-      additionalClassNames.map((className) => list.add(className));
-
-      dispatch('core/block-editor').updateBlockAttributes(clientId, { className: list.value });
-    }
-  })((props) => {
-    let className = 'block ';
-    className += convertToBem(props.className);
-
-    if (editFunction) {
-      if (props.attributes.align && ['full', 'wide'].includes(props.attributes.align)) {
-        className += ' align' + props.attributes.align;
-      }
-
-      return (
-        <EditContext.Provider value={ props }>
-          <div className={ className }>
-            { editFunction(props) }
-          </div>
-        </EditContext.Provider>
-      );
-    }
-
-    const { attributes } = props;
-
-    if (attributes.hasOwnProperty('className')) {
-      delete attributes.className;
-    }
-
-    if (attributes.hasOwnProperty('hideMobile')) {
-      delete attributes.hideMobile;
-    }
-
-    if (attributes.hasOwnProperty('hideDesktop')) {
-      delete attributes.hideDesktop;
-    }
-
+const getEdit = ({ blockName, editFunction }) => ((props) => {
+  if (editFunction) {
     return (
-      <ServerSideRender
-        block={ blockNamespace }
-        attributes={ props.attributes }
-        className={ className }
-      />
+      <EditContext.Provider value={ props }>
+        { editFunction(props) }
+      </EditContext.Provider>
     );
-  })
-);
+  }
+
+  return (
+    <ServerSideRender
+      block={ blockName }
+      attributes={ props.attributes }
+    />
+  );
+});
 
 export default getEdit;
