10d1c174b92b6a1949ad3408265e14a5a86afa21
Kevin Wellmann <kevin@wellmann.io>
2023-10-23 19:45:57 +0200

    fix requireContext with multiple files

--- src/block-registrar.js
+++ src/block-registrar.js
@@ -5,12 +5,29 @@ const { registerBlockType } = wp.blocks;
 // Local dependencies.
 import Edit from './components/Wrapper/EditWrapper';
 
-const requireContext = require.context(__BLOCKS_DIR__, true, /edit\.js$/);
+const blocksToRegister = {};
+const requireContext = require.context(__BLOCKS_DIR__, true, /(block\.json|edit\.js)$/);
 requireContext.keys().forEach((key) => {
   const blockSlug = key.split('/')[1];
+
+  if (!blocksToRegister[blockSlug]) {
+    blocksToRegister[blockSlug] = {};
+  }
+
+  if (key.includes('block.json')) {
+    blocksToRegister[blockSlug].blockSettings = requireContext(key);
+    return;
+  }
+
+  if (key.includes('edit.js')) {
+    blocksToRegister[blockSlug].editFunction = requireContext(key).default;
+    return;
+  }
+});
+
+Object.keys(blocksToRegister).forEach((blockSlug) => {
+  const { blockSettings, editFunction } = blocksToRegister[blockSlug];
   const blockName = __PREFIX__ + '/' + blockSlug;
-  const { default: editFunction } = requireContext(key);
-  const { default: blockSettings } = requireContext(key.replace('edit.js', 'block.json'));
 
   blockSettings.supports = blockSettings.supports || {};
   blockSettings.supports.html = false; // Since save is returning null the blocks HTML can't be edited.
